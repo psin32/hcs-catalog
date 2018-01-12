b6420fa09e708fecf9c45d0c682b5eb234a74fcd
Prashant Singh <psingh@orkutics.com>
2018-01-12 02:40:03 +0000

    Adding swagger API doc generator

--- src/main/java/co/uk/app/commerce/catalog/application/CatalogApplication.java
+++ src/main/java/co/uk/app/commerce/catalog/application/CatalogApplication.java
@@ -1,5 +1,7 @@
 package co.uk.app.commerce.catalog.application;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,9 +12,15 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableEurekaClient
+@EnableSwagger2
 @SpringBootApplication
-@ComponentScan("co.uk.app.commerce.catalog")
+@ComponentScan({ "co.uk.app.commerce.catalog" })
 @EnableMongoRepositories(basePackages = "co.uk.app.commerce.catalog")
 public class CatalogApplication {
 
@@ -31,4 +39,11 @@ public class CatalogApplication {
 		source.registerCorsConfiguration("/**", config);
 		return source;
 	}
+
+	@Bean
+	public Docket swaggerApiDocs() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("co.uk.app.commerce.catalog")).paths(regex("/*.*")).build();
+	}
+
 }
